76fd5bb793070ed9198926800cb9758f2360bece
Dj-Codeman <dwhitfield@ramfield.net>
2022-11-23 01:49:27 -0600

    null

--- install.py
+++ install.py
@@ -58,11 +58,11 @@ def install(keyword):
     os.system(command)
 
     # Unpacking and installing wget
-    command = f"unzip /tmp/encore/pkgs/wget-3.2.zip -d ./"
+    command = f"unzip /tmp/encore/pkgs/wget-3.2.zip"
     os.system(command)
 
 
-    command = f"python3 setup.py install"
+    command = f"python3 ./wget-3.2/setup.py install"
     os.system(command)
 
     import wget
